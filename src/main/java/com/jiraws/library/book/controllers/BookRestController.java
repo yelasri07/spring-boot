484fa94611d97740e85b75a3f7a02107311bcf99
yelasri <elasriyoussef604@gmail.com>
2025-12-01 20:13:23 +0100

    post

--- src/main/java/com/jiraws/library/book/controllers/BookRestController.java
+++ src/main/java/com/jiraws/library/book/controllers/BookRestController.java
@@ -2,12 +2,14 @@ package com.jiraws.library.book.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jiraws.library.book.services.BookService;
 
 @RestController
+@RequestMapping("/book")
 public class BookRestController {
 
     private final BookService bookService;
@@ -16,15 +18,15 @@ public class BookRestController {
         this.bookService = bookService;
     }
 
-    @GetMapping("/book")
-    public String get(@RequestParam String bookName, @RequestParam Integer bookPages) {
+    @GetMapping
+    public String get() {
 
-        return this.bookService.createBook(bookName, bookPages);
+        return "books";
 
     }
 
-    @PostMapping("/book")
-    public String post() {
-        return "hello post";
+    @PostMapping
+    public String post(@RequestParam String bookName, @RequestParam Integer bookPages) {
+        return this.bookService.createBook(bookName, bookPages);
     }
 }
